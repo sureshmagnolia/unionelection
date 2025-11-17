7136e2730f576b7e32c5f3091a07f0fe785c631c
sureshmagnolia <118750471+sureshmagnolia@users.noreply.github.com>
2025-11-17 22:45:08 +0530

    Update app.js

--- scr4/app.js
+++ scr4/app.js
@@ -1012,7 +1012,7 @@ generateDaywiseReportButton.addEventListener('click', async () => {
         let allPagesHtml = '';
         let totalPagesGenerated = 0;
         // *** FIX: Changed to 35 rows per column for A4 fit ***
-        const STUDENTS_PER_COLUMN = 30; 
+        const STUDENTS_PER_COLUMN = 25; 
         const COLUMNS_PER_PAGE = 2; 
         const STUDENTS_PER_PAGE = STUDENTS_PER_COLUMN * COLUMNS_PER_PAGE; 
 
